dca8023d7e5a3d74002246873dd0a82ccddee89f
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-04-27 09:05:21 +0300

    calclEnd

--- cacl/calc.js
+++ cacl/calc.js
@@ -1,11 +1,21 @@
 "use strict"; // here we go again
 
 import add, { blue, red as purple } from "./add.js";
+import { a as a, b, c, d as tomato } from "./var.js";
 // pervadinam kintamaji
 import { mod as modulus } from "./mod.js";
-
+// a = 1000; // error
 add(10, 20);
 modulus(24, 7);
 
 console.log("blue", blue, true);
 console.log("purple", purple, true);
+
+console.group("Veiksmai su itrauktais kintamaisiais");
+add(a, b);
+add(c, tomato);
+modulus(c, tomato);
+console.log("a + b + c + tomato", a + b + c + tomato);
+
+console.groupEnd();
+console.log("");
---
+++ cacl/var.js
@@ -0,0 +1,13 @@
+"use strict"; // here we go again
+
+let a = 50;
+
+export const b = 100;
+
+const c = 70;
+
+export const d = 10;
+
+// galima exoportuoti po visko
+
+export { a, c };
